9940665cc46e750b2eb929f1954d35852f308b34
Kévin Camus <kcamus@sio.local>
2023-11-03 11:17:30 +0100

    .

--- docs/assets/js/exemple.js
+++ docs/assets/js/exemple.js
@@ -10,4 +10,11 @@ const keyPressFunction=function(event) {
 
 document.addEventListener('DOMContentLoaded', function() {
     addEvt('#nom', 'keyup', keyPressFunction);
+    addEvt('#bt-creer', 'click', ()=>{
+        const li=document.createElement('li');
+        li.innerText=$('#elm').value;
+        li.style=$('#color').value;
+        $('#ul-elements').appendChild(li);
+
+    });
 });
