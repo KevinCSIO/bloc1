40ea2d7a3e8e4d9f44a2d45f77bce3395d7ec841
Kévin Camus <kcamus@sio.local>
2023-11-03 10:47:39 +0100

    ..

--- docs/assets/js/td4/exemple.js
+++ docs/assets/js/td4/exemple.js
@@ -1,7 +1,13 @@
-const keyPressFunction=function(event) {
-    document.querySelector('#saisie').innerHTML=event.target.value;
-    };
+const $=(selector)=>document.querySelector(selector);
 
-document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('#nom').addEventListener('keyup', keyPressFunction);
+const addEvt=(selector, type, callback)=>{
+    $(selector).addEventListener(type, callback);
+};
+
+const keyPressFunction=function(event) {
+    $('#saisie').innerHTML=event.target.value;
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    addEvt('#nom', 'keyup', keyPressFunction);
 });
