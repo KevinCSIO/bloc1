cd2c7f07a7a5ae38b986fb3382b9021c5424647e
Kévin Camus <kcamus@sio.local>
2023-11-21 15:29:08 +0100

    background-color

--- docs/assets/js/exercices.js
+++ docs/assets/js/exercices.js
@@ -6,3 +6,18 @@ const addEvt=(selector, type, callback)=>{
         elm.addEventListener(type, callback);
     }
 };
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Sélectionner le bouton et ajouter un gestionnaire d'événements au clic
+    var colorButton = document.getElementById('colorButton');
+    colorButton.addEventListener('click', changeColor);
+
+    // Fonction pour changer la couleur du fond de la page
+    function changeColor() {
+        // Générer une couleur aléatoire au format hexadécimal
+        var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+        
+        // Appliquer la nouvelle couleur au fond de la page
+        document.body.style.backgroundColor = randomColor;
+    }
+});
