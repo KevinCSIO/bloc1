80dae196eafe32fbd3ee7e6bf20e5ce03068c1e5
Kévin Camus <kcamus@sio.local>
2023-11-23 11:53:05 +0100

    .

--- docs/td5/Lorem-ipsum-gen.php
+++ docs/td5/Lorem-ipsum-gen.php
@@ -1,15 +1,32 @@
 <?php
-require "fonctions.php";
+include_once 'fonctions.php'
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lorem Ipsum generator</title>
+</head>
+<body>
+    <form method="post">
+    <label for="nb">Nombre de paragraphes</label>
+    <input type="number" value="5" name="count" id="nb">
+    </form>
+<?php
+$nb=$_POST['count'];
+$nbElementsInIpsum=count(IPSUM_ARRAY);
+$j=0;
+for($i=0;$i<$nb;$i++){
+    $paragraphe=IPSUM_ARRAY[$j];
+    $j++;
+    titre("Para ".($i+1), level: 6);
+    if($j==$nbElementsInIpsum){
+        $j=0;
+    }
+    echo "<div>$paragraphe</div>";
+}
+?> 
+</body>
+</html>
 
-$a=[1,2,3,"Bonjour"];
-titre("Fonction count");
-echo count($a). " élément(s) dans a";
-titre("Parcours");
-titre("Avec foreach", level: 2);
-foreach ($a as $i=>$v){
-    echo "<br>".$v." à la position ".$i;
-}
-titre("For classique", "Plus long...", 2);
-for($i=0;$i<count($a);$i++){
-    echo "<br>$a[$i] à la position $i";
-}
