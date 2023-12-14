50d95763d9b6fc79aeb4a86dbf21986636da54db
Kévin Camus <kcamus@sio.local>
2023-12-14 11:59:29 +0100

    rappels

--- docs/rappels/index.php
+++ docs/rappels/index.php
@@ -2,8 +2,12 @@
 include_once "src/gui.php";
 titre("Niveau 1");
 titre("Teste de niveau 2",2);
-
-$message="Bonjour";
+?>
+<form method="post">
+    <input name="message" placeholder="Votre message">
+</form>
+<?php
+$message=post('message','pas de msg');
 echo $message;
 ?>
 <?=titre($message,6)?>
--- docs/rappels/src/gui.php
+++ docs/rappels/src/gui.php
@@ -2,3 +2,6 @@
 function titre(string $content, int $niveau=1):void {
     echo "<h$niveau>$content</h$niveau>";
 }
+function post(string $name,$default=null){
+    return $_POST[$name]??$default;
+}
